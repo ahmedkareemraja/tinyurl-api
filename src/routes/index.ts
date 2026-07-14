90478c181a0c2f598b0d2ca25697bcff50ecd66e
Ahmed Kareem Raja <ahmedkareemraja123@gmail.com>
2026-07-14 12:02:05 +0500

    initial commit

---
+++ src/routes/index.ts
@@ -0,0 +1,41 @@
+import { Express, NextFunction, Request, Response, Router } from "express";
+
+interface RouteDefinition {
+    path: string;
+    router: Router;
+}
+
+const routerConfig: {
+    publicRoutes: RouteDefinition[];
+    privateRoutes: RouteDefinition[];
+} = {
+    publicRoutes: [
+        // { path: "", router: undefined },
+    ],
+    privateRoutes: [
+        // { path: "", router: undefined },
+    ],
+};
+
+function authenticateToken(req: Request, res: Response, next: NextFunction) {
+    // TODO: implement token verification
+    next();
+}
+
+export default (app: Express) => {
+    const routerPublic = Router();
+    const routerPrivate = Router();
+
+    routerPrivate.use(authenticateToken);
+
+    routerConfig.publicRoutes.forEach((route) => {
+        routerPublic.use(route.path, route.router);
+    });
+
+    routerConfig.privateRoutes.forEach((route) => {
+        routerPrivate.use(route.path, route.router);
+    });
+
+    app.use(routerPublic);
+    app.use(routerPrivate);
+};
---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const { PORT, DB_CONNECTION_STRING } = process.env;
+
+if (!DB_CONNECTION_STRING) {
+    throw new Error("Missing DB_CONNECTION_STRING environment variable");
+}
+if (!PORT) {
+    throw new Error("Missing PORT environment variable");
+}
+
+mongoose.connect(DB_CONNECTION_STRING).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}).catch((err) => {
+    console.log(err);
+});
