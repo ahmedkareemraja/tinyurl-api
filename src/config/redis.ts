592c4eea7ed8118f65c25c5b8aaf4ccdd692747c
Ahmed Kareem Raja <ahmedkareemraja123@gmail.com>
2026-07-22 01:06:16 +0500

    setup redis client

---
+++ src/config/redis.ts
@@ -0,0 +1,17 @@
+import { createClient } from 'redis';
+
+import logger from '../utils/logger';
+
+const redisClient = createClient({
+  url: process.env['REDIS_URL'] ?? 'redis://localhost:6379',
+});
+
+redisClient.on('error', (err: unknown) => {
+  logger.error('Redis client error:', err);
+});
+
+redisClient.connect().catch((err: unknown) => {
+  logger.error('Error connecting to Redis:', err);
+});
+
+export default redisClient;
