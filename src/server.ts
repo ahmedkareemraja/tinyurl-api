6955984b1e094decacfac407e99d49654aa4faa4
Ahmed Kareem Raja <ahmedkareemraja123@gmail.com>
2026-07-21 19:08:43 +0500

    added rate limiter

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 
 import express from 'express';
+import { rateLimit, MINUTE } from 'express-rate-limit';
 import mongoose from 'mongoose';
 
 import passport from './config/passport';
@@ -13,6 +14,18 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
+
+const limiter = rateLimit({
+  windowMs: 1 * MINUTE, // SECOND, MINUTE, HOUR, and DAY constants are available, or a use bare number for milliseconds
+  limit: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+  // store: ... , // Redis, Memcached, etc. See below.
+});
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter);
 
 routes(app);
 
